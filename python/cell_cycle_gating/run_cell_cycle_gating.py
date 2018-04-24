7e77d1764284e3d92f300c078439110b374a22e7
subkar <sub.kartik@gmail.com>
2018-04-24 16:36:42 -0400

    this update allows user to pass metadata file as additional argument. As a result, the output plots are named and grouped by cell line, drug and concentration

--- python/cell_cycle_gating/run_cell_cycle_gating.py
+++ python/cell_cycle_gating/run_cell_cycle_gating.py
@@ -9,35 +9,44 @@ from cell_cycle_gating import cellcycle_phases as cc
 from cell_cycle_gating import ph3_filter as pf
 
 
-def run(object_level_directory):
+def run(object_level_directory, dfm=None):
     """ Executes cell cycle gating on all wells for which object level
     data is available in object_level_directory. Plots and saves summary pdf
     of DNA v EdU distribution with automated gatings. A dataframe summarizing
-    results for each well is also saved
+    results for each well is also saved.
+
     Parameters
     ----------
-    object_level_directory: str
+    object_level_directory : str
         name of folder containing object level data for a single plate
+    dfm : Optional[pandas dataframe]
+        metadata table of experimental design. Default is None.
+
     Returns
     -------
-    df: pandas dataframe
+    df : pandas dataframe
        well level summary of number of live/dead cells and fraction of
        cells in each phase of the cell cycle
     """
     plt.ioff()
-    object_level_files = [s for s in os.listdir(object_level_directory)
-                          if 'Nuclei Selected[0].txt' in s]
+    if dfm is not None:
+        dfm_ord = merge_metadata(dfm, object_level_directory)
+        object_level_files = dfm_ord['object_level_file'].tolist()
+    else:
+        object_level_files = [s for s in os.listdir(object_level_directory)
+                              if 'Nuclei Selected[0].txt' in s]
 
     df_summary = pd.DataFrame()
     nb_plots = len(object_level_files)
     pdf_pages = PdfPages('summary_%s.pdf' % object_level_directory)
     nb_plots_per_page = 10
     # nb_pages = int(np.ceil(nb_plots / float(nb_plots_per_page)))
-    for i, file in enumerate(object_level_files[:nb_plots]):
+    for i, file in enumerate(object_level_files):
         if i % nb_plots_per_page == 0:
             fig = plt.figure(figsize=(8.27, 11.69), dpi=100)
         df = pd.read_table('%s/%s' % (object_level_directory, file))
         well = re.search('result.(.*?)\[', file).group(1)
+        well = "%s%s" % (well[0], well[1:].zfill(2))
 
         edu = np.array(df['Nuclei Selected - EdUINT'].tolist())
         dna = np.array(df['Nuclei Selected - DNAcontent'].tolist())
@@ -56,8 +65,16 @@ def run(object_level_directory):
             dna_gates = dcf.get_dna_gating(dna, ldr, ldr_gates)
             a, d, _ = dcf.live_dead(ldr, ldr_gates, dna, dna_gates)
             # Get phases based on DNA and EdU
+            if dfm is not None:
+                # dfm_ord.index = dfm_ord.well
+                cell_line = dfm_ord.loc[well, 'cell_line']
+                agent = dfm_ord.loc[well, 'agent']
+                conc = dfm_ord.loc[well, 'concentration']
+                title = "%s %s %s (%s um)" % (well, cell_line, agent, conc)
+            else:
+                title = well
             fractions, cell_identity = cc.plot_summary(dna, edu, fig,
-                                                       well=well,
+                                                       title=title,
                                                        plot='scatter',
                                                        plot_num=i)
 
@@ -83,9 +100,52 @@ def run(object_level_directory):
         if (i + 1) % nb_plots_per_page == 0 or (i + 1) == nb_plots:
             plt.tight_layout()
             pdf_pages.savefig(fig)
+            print("Completed analysis for %d out of %d wells" %
+                  (i+1, len(object_level_files)))
+            plt.close('all')
     pdf_pages.close()
     df_summary = df_summary[['well', 'total', 'alive', 'dead',
                              'G1', 'S', 'G2', 'M',
                              'S_dropout', 'other']]
+    if dfm is not None:
+        df_summary.index = df_summary.well.tolist()
+        df_summary = pd.concat([dfm_ord, df_summary], axis=1)
     df_summary.to_csv('summary_%s.csv' % object_level_directory, index=False)
     return df_summary
+
+
+def merge_metadata(dfm, obj):
+    """Merges metadata with object level file and
+    sorts files by cell line, drug and concetration.
+
+    Parameters
+    ----------
+    dfm : pandas dataframe
+        metadata file with wells mapped to treatment conditions
+    obj : str
+        path to folder containing object level data
+
+    Returns
+    -------
+    dfmc : pandas dataframe
+        mapping between wells, treatment, and filename
+        containing object level data.
+        Sorted by cell line, drug and concentration
+    """
+    barcode = obj.split('[')[0]
+    dfm = dfm[dfm.barcode == barcode].copy()
+    dfm.index = dfm.well.tolist()
+    wells = []
+    object_level_files = [s for s in os.listdir(obj)
+                          if 'Nuclei Selected[0].txt' in s]
+    for file in object_level_files:
+        well = re.search('result.(.*?)\[', file).group(1)
+        well = "%s%s" % (well[0], well[1:].zfill(2))
+        wells.append(well)
+    dfmap = pd.DataFrame(list(zip(wells, object_level_files)),
+                         columns=['well', 'object_level_file'])
+    dfmap.index = dfmap.well.tolist()
+    dfmc = pd.concat([dfm, dfmap], axis=1)
+    dfmc = dfmc.dropna(subset=['object_level_file'])
+    dfmc = dfmc.sort_values(['cell_line', 'agent', 'concentration'])
+    return dfmc
