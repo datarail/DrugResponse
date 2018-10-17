ff2e8347aac8cf2f313b6163b37c184335ecccfb
yunguan-wang <YUNGUAN_WANG@HMS.HARVARD.EDU>
2018-10-17 15:44:39 -0400

    updated functionality

--- python/cell_cycle_gating/plotting.py
+++ python/cell_cycle_gating/plotting.py
@@ -1,30 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def plot_stacked_bar(data,
+def plot_stacked_bar(data, ax=None,
     data_cols = ['G1', 'S', 'G2', 'S_dropout','other', 'M'],
-    metadata_cols = ['agent','cell_line','well'],
+    condition_cols = ['agent','cell_line','well'],
     figname = 'Stack_bar_plot.png',
     stacked = True,
-    title = None):
+    title = None,
+    show_legend = True,
+    **kwargs):
     '''
     Make stacked barplot. Figure is saved to the current working folder.
     Parameters
     ========
     Todo
     '''
-    if isinstance(metadata_cols,str):
-        data.set_index(metadata_cols, inplace=True)
+    ax = ax or plt.gca()
+    if isinstance(condition_cols,str):
+        data.set_index(condition_cols, inplace=True)
     else:
-        data.set_index(data[metadata_cols].apply(lambda x: '_'.join(x.values),axis = 1).values, inplace=True)
-    g = data[data_cols].plot(kind='bar', stacked=stacked)
-    plt.legend(bbox_to_anchor = (1,0.7))
-    plt.xticks(rotation=45)
+        data.set_index(data[condition_cols].apply(lambda x: '_'.join(x.values),axis = 1).values, inplace=True)
+    g = data[data_cols].plot(kind='bar', stacked=stacked, ax=ax, **kwargs)
+    if show_legend:
+        plt.legend(bbox_to_anchor = (1,0.7))
+    ax.tick_params(labelsize=16)
+    ax.tick_params(axis = 'x',labelrotation=45)
     plt.ylabel('Fraction')
     if title is not None:
         plt.title(title)
     if figname is None:
-        plt.show()
+        return g
     else:
         plt.tight_layout()
         plt.savefig(figname)
@@ -32,13 +38,36 @@ def plot_stacked_bar(data,
 
 def batch_stacked_bar_plot(data,
     data_cols = ['G1', 'S', 'G2', 'S_dropout','other', 'M'],
-    condition_col = ['agent','cell_line','well'],
+    row_by = 'agent',
+    col_by = 'cell_line',
     plot_x_col = 'concentration',
-    stacked = True):
-    data['condition'] = data[condition_col].apply(lambda x: '_'.join(x.values), axis = 1)
-    all_conditions = data.condition.unique()
-    for condition in all_conditions:
-        condition_data = data[data.condition==condition].iloc[:,:-1]
+    stacked = True,
+    savefig = False,
+    **kwargs):
+    data.sort_values([row_by,col_by,plot_x_col],inplace=True)
+    nrows = data[row_by].unique().shape[0]
+    ncols = data[col_by].unique().shape[0]
+    fig, axes = plt.subplots(nrows,ncols,sharex=True, sharey=True,**kwargs)
+    axes = axes.ravel()
+    plot_idx = 0
+    for i,row_value in enumerate(data[row_by].unique()):
+        for j,col_value in enumerate(data[col_by].unique()):
+            plot_data = data[(data[row_by]==row_value) & (data[col_by]==col_value)]
+            plot_stacked_bar(plot_data, ax=axes[plot_idx], data_cols=data_cols, condition_cols=plot_x_col, figname=None,stacked=stacked, show_legend = False)
+            axes[plot_idx].set_title('{}:{}'.format(str(row_value),str(col_value)))
+            axes[plot_idx].legend([])
+            plot_idx+=1
+    if nrows>=ncols:
+        legend_loc = (1.1,-0.5)
+        plt.legend(bbox_to_anchor = legend_loc,ncol=len(data_cols))
+    else:
+        legend_loc = (1,0.7)
+        plt.legend(bbox_to_anchor = legend_loc)
+    plt.ylabel('Fraction')
+    if savefig:
         figname = '_'.join([condition,'stacked_barplot.png'])
-        condition_data.sort_values(plot_x_col,inplace=True)
-        plot_stacked_bar(condition_data,data_cols=data_cols, metadata_cols=plot_x_col, figname=figname,stacked=stacked, title=condition)
+        plt.savefig(figname)
+    else:
+        plt.show()
+    plt.close()
+    return
