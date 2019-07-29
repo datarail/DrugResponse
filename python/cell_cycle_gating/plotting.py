05e85f9aa26113bce3e2b667dc21cbde8710a48f
smkartik <sub.kartik@gmail.com>
2019-07-29 17:25:32 -0400

    completely refactor plotting functions to return DNA content distributions across dose concentrations

--- python/cell_cycle_gating/plotting.py
+++ python/cell_cycle_gating/plotting.py
@@ -3,6 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
+import os
+from matplotlib.collections import PolyCollection
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+from cell_cycle_gating import dead_cell_filter as dcf
+
 
 
 def plot_stacked_bar(data, ax=None,
@@ -103,24 +111,113 @@ def batch_stacked_bar_plot(data,
     return
 
 
-def group_hist3d(data, figsize=(8, 8), title=None, figname=None):
+def group_hist3d(dna_content, figsize=(8, 8), title=None, plot_num=0, figname=None):
     '''
     Make stacked histogram plots, each column is plotted as a series.
     '''
-    fig = plt.figure(figsize=figsize)
-    ax = plt.axes(projection='3d')
-    for i in range(data.shape[1]):
-        hist_y, hist_x = np.histogram(data.iloc[:, i], 100, density=True)
-        hist_x = hist_x[:-1] + np.diff(hist_x[:2])
-        ax.bar3d(hist_x, i, 0, 0.1, 0.05, hist_y, shade=False, alpha=0.5)
+    #fig = plt.figure(figsize=figsize)
+    #ax = plt.axes(projection='3d')
+    #fig = plt.figure(figsize=(8, 4))
+    grid_size=(2, 1)
+    plot_num = plot_num % 2
+    rel_pos = np.unravel_index(plot_num, grid_size)
+    ax = plt.subplot2grid(grid_size, rel_pos, projection='3d')
+    doses = list(dna_content.keys())
+    verts = []
+    for i, conc in enumerate(doses):
+        hist_y, hist_x = np.histogram(dna_content[conc], 100, density=True)
+        hist_x = hist_x[:-1]# + np.diff(hist_x[:2])
+        verts.append(list(zip(hist_x, hist_y)))
+        a = 0.1 * (i+1)
+        #ax.bar3d(hist_x, i, 0, 0.1, 0.05, hist_y, color=(0.2, 0.7, 0.3, a), shade=False)#, alpha=0.2)
+    poly = PolyCollection(verts, edgecolors='k')#, facecolors=[cc('r'), cc('g'), cc('b'), cc('y')])
+    poly.set_alpha(0.7)
+    ax.add_collection3d(poly, zs=np.arange(len(doses)), zdir='y')
+
+    dna_content_values = []
+    for k in dna_content.keys():
+        dna_content_values += list(dna_content[k])
+    
+    ax.set_xlabel('log10(DNA content)')
+    ax.set_xlim3d(4, 5)
+    ax.set_ylabel('doses')
+    ax.set_ylim3d(0, 9)
+    #ax.set_zlabel('Z')
+    ax.set_zlim3d(0, 5)
+    ax.view_init(elev=67, azim=-45)
+    ax.set_yticklabels(doses, fontsize=8)
     if title is not None:
         ax.set_title(title, fontsize=16)
-    ax.set_xlabel('DNA Content', fontsize=16)
-    ax.set_zlabel('Density', fontsize=16)
-    ax.set_yticks(np.arange(data.shape[1]))
-    ax.set_yticklabels(data.columns.values, fontsize=16)
-    ax.tick_params(axis='y', labelrotation=15)
-    if figname is not None:
-        plt.savefig(figname)
-    else:
-        return fig
+    #if figname is not None:
+    #    plt.savefig(figname)
+    #else:
+    #    return fig
+
+
+
+ #def collate_wells(dfm, barcode):
+ #    dfm = dfm[dfm.barcode == barcode].copy()
+ #    conditions = list(zip(dfm.cell_line, dfm.agent))
+ #    conditions = list(set(conditions))
+ #    for condition in conditons:
+def collate_wells(dfm, cell_line, agent, barcode=None):
+     if barcode:
+         dfm = dfm[dfm.barcode == barcode].copy()
+     da = dfm[(dfm.cell_line == cell_line) & (dfm.agent == agent)][
+             ['barcode', 'cell_line', 'agent', 'concentration', 'well']].copy()
+     da = da.sort_values('concentration')
+     dna_content = {}
+     for conc in da.concentration.unique():
+         wells = da[da.concentration == conc]['well'].tolist()
+         barcodes = da[da.concentration == conc]['barcode'].tolist()
+         wells = ["%s%s" % (w[0], int(w[1:])) for w in wells]
+         dna_content[conc] = get_dna_content(barcodes, wells)
+     return dna_content
+
+
+def get_dna_content(barcodes, wells):
+    dna_content = []
+    for bc, well in zip(barcodes, wells):
+        try:
+            bc = [s for s in os.listdir('.') if bc in s][0]
+        except IndexError:
+            print("data folder with barcode %s does not exist" % bc)
+        data_file = [s for s in os.listdir(bc) if 'result.%s[' % well in s][0]
+        df = pd.read_table("%s/%s" % (bc, data_file))
+        dna = np.array(df['Nuclei Selected - DNAcontent'].tolist())
+        log_dna = dcf.compute_log_dna(dna)
+        dna_content += list(log_dna)
+    return np.array(dna_content)    
+
+
+
+def plot(dfm, barcode=None, figname='test.pdf'):
+    dfm = dfm[(dfm.cell_line.notnull()) & (dfm.agent.notnull())]
+    if barcode:
+         dfm = dfm[dfm.barcode == barcode].copy()     
+    conditions = list(zip(dfm.cell_line, dfm.agent))
+    conditions = list(set(conditions))
+    nb_plots = len(conditions)
+    pdf_pages = PdfPages(figname)
+    nb_plots_per_page = 3
+
+    for i, cond in enumerate(conditions):
+        if i % nb_plots_per_page == 0:
+            fig = plt.figure(figsize=(8.27, 11.69), dpi=100)
+        cell_line = cond[0]
+        agent = cond[1]
+        title =  "%s %s" % (cell_line, agent)
+        dna_content = collate_wells(dfm, cell_line, agent)
+        group_hist3d(dna_content, title=title, plot_num=i)
+        
+        if (i + 1) % nb_plots_per_page == 0 or (i + 1) == nb_plots:
+           plt.tight_layout()
+           pdf_pages.savefig(fig)
+           print("Completed analysis for %d out of %d conditions" %
+                 (i+1, len(conditions)))
+           plt.close('all')
+    pdf_pages.close()       
+
+             
+         
+                  
