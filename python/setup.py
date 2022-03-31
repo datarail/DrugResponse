f5730795aaeb9b0ffa2689a1643f4ed9541fa58a
Nick Clark <nicholas.clark00@gmail.com>
2022-03-31 12:33:45 -0400

    add dependencies

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ def main():
           author_email='marc_hafner@hms.harvard.edu',
           url='http://github.com/datarail/DrugResponse',
           packages=find_packages(),
-          install_requires=['numpy', 'pandas', 'peakutils'],
+          install_requires=['numpy', 'pandas', 'peakutils', 'seaborn', 'matplotlib'],
           keywords=['systems', 'biology', 'data', 'array', 'matrix'],
           classifiers=[
             'Development Status :: 3 - Alpha',
